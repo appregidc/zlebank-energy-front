f8a02a257131d428f3ba795ad1cc42224253f745
feifei135 <1475961677@qq.com>
2016-09-12 16:43:59 +0800

    互金所有页面

---
+++ js/motai.js
@@ -0,0 +1,32 @@
+//提示框,需传一个参数，参数cot为提示内容
+function alerts(cot){ 
+	  $(".alert_cont").text(cot);
+	  layer.open({
+		  type: 1,
+		  //shadeClose:true,
+		  // shade:0,
+		  id:'a',
+		  tipsMore:true,
+		  title:  ['提示', 'border-bottom: 1px solid #ddd;font-size: 16px;color: #666;'],
+		  area: ['400px', '230px'], //宽高
+		  content: $(".alert_box")
+	  });
+	  $(".alert_btn").click(function(){layer.closeAll();});
+}
+//模态窗口，需传两个参数，参数cot为提示内容,href为网页跳转地址
+function motai(cot,href){ 
+	$(".motai_cont").text(cot);
+	layer.open({
+		type: 1,
+		id:'a',
+		title:  ['提示', 'border-bottom: 1px solid #ddd;font-size: 16px;color: #666;'],
+		area: ['400px', '230px'], //宽高
+		content: $(".motai_box")
+	});
+	$(".motai_cancel").click(function(){
+		layer.closeAll();
+	});
+	$(".motai_sure").click(function(){
+		location.href=href;
+	});
+}
